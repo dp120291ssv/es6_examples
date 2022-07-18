58121c9fba06c40b40c36267308cbaa781f50022
Sergei Semenov <dp120291ssv@gmail.com>
2022-07-18 20:03:42 +0300

    var let const

---
+++ app/js/common.js
@@ -0,0 +1,29 @@
+/**
+ * 1. let, var
+ */
+
+// a) Scope
+
+// if(true){
+//     var a = 12;
+//     let a = 12;
+// }
+// console.log(a)
+
+// b) Hoisting
+
+// console.log(b)
+// var b = 7;
+
+
+/**
+ * 2. const
+ * */
+
+// const ARR = [1, 2, 3]
+// ARR.push(4)
+// console.log(ARR)
+
+// const OBJ = {"es": 5}
+// OBJ.es = 6;
+// console.log(OBJ)
