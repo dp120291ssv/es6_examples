716f0d19482480dbdde742a99622da0cc2f87b52
Sergei Semenov <dp120291ssv@gmail.com>
2022-07-20 19:42:40 +0300

    template strings

---
+++ app/js/common.js
@@ -0,0 +1,26 @@
+// Example 1 - string
+// function greeting(name) {
+//     console.log('Hello ' + name + '!');
+//     console.log(`Hello ${name}!`);
+// }
+// greeting('JS');
+
+// Example 2 - URL API string
+// const URL = '/api';
+// let path1 = 'test1';
+// let path2 = 'test2';
+// let path3 = 'test3';
+// const API_URL = URL + '/' + path1 + '/' + path2 + '/' + path3;
+// const API_URL2 = `${URL}/${path1}/${path2}/${path3}`;
+// console.log(API_URL)
+// console.log(API_URL2)
+
+// Example 3 - Multiple string
+// function emailMsg(name, mail, status) {
+//     return `
+//         Hello ${name},
+//         Your email ${mail} has been ${status}!
+//         Thank you for subscription!
+//     `
+// }
+// console.log(emailMsg('Sergei', 'dp120291ssv@gmail.com', 'approved'))
