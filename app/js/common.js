d3a48e094435e21efef29a98783d84ce0a40cf6d
Sergei Semenov <dp120291ssv@gmail.com>
2022-07-21 13:29:30 +0300

    loop for..of

---
+++ app/js/common.js
@@ -0,0 +1,14 @@
+// for..of
+const names = ['Jack', 'Sam', 'Mark']
+//
+// for(let name in names){
+//     console.log(name)
+// }
+//
+// for(let index in names){
+//     console.log(names[index])
+// }
+
+for(let name of names){
+    console.log(name)
+}
